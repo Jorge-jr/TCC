5c8d649ae9f00185c1b5bc0cbf013165351e0339
Jorge-jr <jsribeirojr@hotmail.com>
2021-04-29 01:29:04 -0300

    Create snarf.py
    Programa que controla o canal a ser monitorado, chama a fubção sniff() e encaminha os pacotes filtrados para o controlador.

---
+++ snarf.py
@@ -0,0 +1,32 @@
+import threading, os, time, datetime, random, socket
+from scapy.all import *
+
+
+def hopper(iface):
+  
+  channel = 11
+  stop_hopper = False
+  
+  while not stop_hopper:
+    time.sleep(0.5)
+    os.system(f"iwconfig {iface} channel {channel})  # configura o canal a ser monitorado
+    print(f"current channel: {channel}", end='\r')
+    channel = int(random.randint(1,14))  próximo canal escolhido aleatóriamente
+
+
+def frame_reporter(pky):
+  if pkt.type == 0 and pkt.subtype == 4:  # filtrando probe requests
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # CRIA UM SOCKET (ipv4, tcp)
+    server.connect()
+    server.connect(('192.198.0.127', 1234))
+    server.send(bytes(pkt.addr2 + '|' + datetime.datetime.now()), "utf-8")
+    print(f"{pkt.addr2} enviado")
+    server.shutdown(socket.SHUT_RDWR)
+    server.close()
+
+
+if name == "__main__":
+  thread = threading.Thread(target=hopper, args=("mon0",), name="hopper")
+  thread.daemon = True
+  thread.start()
+  sniff(iface="mon0", prn=frame_reporter)
