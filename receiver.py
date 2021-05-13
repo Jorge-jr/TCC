b17149dba13934b11eb40014f7c073214934a6d0
Jorge-jr <jsribeirojr@hotmail.com>
2021-05-13 09:29:05 -0300

    receiver.py - corrigido timestamp

--- receiver.py
+++ receiver.py
@@ -1,4 +1,4 @@
-import socket, station
+import socket, station, datetime
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -12,20 +12,30 @@ stations = []
 while True:
 	con, client = s.accept()
 	print(f"Conectado a {client}")
-	msg = con.recv(1024)
-	
+	msg = con.recv(1024).decode('utf-8')
+
 	print(f"{client}: {msg}")
 	print(f"encerrando conexão com {client}")
 	con.shutdown(socket.SHUT_RDWR)
 	con.close()
 
-    for station in stations:
-        if station.get_address() == msg.split('|')[0]:
-            station.new_detection(msg.split('|')[1])
-            break
-    else:
-        stations +=  [station.Station(msg.split('|')[0], msg.split('|')[1])]
+	time_string = msg.split('|')[1]
+	year = int(time_string.split('-')[0])
+	month = int(time_string.split('-')[1])
+	day = int(time_string.split('-')[2].split(' ')[0])
+	#hour = int(a.split('-')[2].split(' ')[0].split(':')[0])
+	hour = int(time_string.split(' ')[1].split(':')[0])
+	minute = int(time_string.split(' ')[1].split(':')[1])
+	second = int(time_string.split(' ')[1].split(':')[2].split('.')[0])
+	detection_time = datetime.datetime(year, month, day, hour, minute, second)
 
 
-    stations += [Station()]
-    
+	for st in stations:
+		if st.get_address() == msg.split('|')[0]:
+			st.new_detection(detection_time)
+			break
+	else:
+		stations +=  [station.Station(msg.split('|')[0], detection_time)]
+                #print(msg.split('|')[0], msg.split('|')[1])
+
+	print(len(stations))
